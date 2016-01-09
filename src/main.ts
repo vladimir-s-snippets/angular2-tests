fb53dae00289c800d255576940609dfe5dd3bbb3
Statsenko Vladimir <vovchiksta@gmail.com>
2016-01-09 17:00:13 +0200

    Angular2 quickstart

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,32 @@
 /*
  * Providers provided by Angular
  */
-import {provide} from 'angular2/core';
-import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
-import {HTTP_PROVIDERS} from 'angular2/http';
+// import {provide} from 'angular2/core';
+// import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
+// import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+// import {HTTP_PROVIDERS} from 'angular2/http';
 
-/*
- * App Component
- * our top level component that holds all of our components
- */
-import {App} from './app/app';
-/*
- * Bootstrap our Angular app with a top level component `App` and inject
- * our Services and Providers into Angular's dependency injection
- */
-document.addEventListener('DOMContentLoaded', function main() {
-  bootstrap(App, [
-    ...('production' === process.env.ENV ? [] : ELEMENT_PROBE_PROVIDERS),
-    ...HTTP_PROVIDERS,
-    ...ROUTER_PROVIDERS,
-    provide(LocationStrategy, { useClass: HashLocationStrategy })
-  ])
-  .catch(err => console.error(err));
-});
+// /*
+//  * App Component
+//  * our top level component that holds all of our components
+//  */
+// import {App} from './app/app';
+// /*
+//  * Bootstrap our Angular app with a top level component `App` and inject
+//  * our Services and Providers into Angular's dependency injection
+//  */
+// document.addEventListener('DOMContentLoaded', function main() {
+//   bootstrap(App, [
+//     ...('production' === process.env.ENV ? [] : ELEMENT_PROBE_PROVIDERS),
+//     ...HTTP_PROVIDERS,
+//     ...ROUTER_PROVIDERS,
+//     provide(LocationStrategy, { useClass: HashLocationStrategy })
+//   ])
+//   .catch(err => console.error(err));
+// });
+
+import {bootstrap}    from 'angular2/platform/browser'
+
+import {AppComponent} from './app/app.component'
+
+bootstrap(AppComponent);
