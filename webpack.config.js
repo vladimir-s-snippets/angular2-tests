c6e2ead5aa34610ba38cca68403c2c557970c4e8
Vladimir Statsenko <vovchiksta@gmail.com>
2016-01-09 12:12:15 +0200

    Config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,113 @@
+var path = require('path');
+
+var ProvidePlugin = require('webpack/lib/ProvidePlugin');
+var DefinePlugin = require('webpack/lib/DefinePlugin');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+
+var metadata = {
+    title: 'angular2 tests',
+    baseUrl: '/',
+    host: 'localhost',
+    port: 3000,
+    ENV: ENV
+};
 
 module.exports = {
-    context: __dirname + "/src",
-	entry: './ts/boot.ts',
-
-	output: {
-		path: __dirname + '/build',
-		publicPath: '/',
-		filename: 'js/[name].js',
-        chunkFilename: '[id].chunk.js'
-	},
-    
-    watch: true,
-    
+    // static data for index.html
+    metadata: metadata,
+    // for faster builds use 'eval'
     devtool: 'source-map',
+    debug: true,
+
+    entry: {
+        'vendor': './src/vendor.ts',
+        'main': './src/boot.ts' // our angular app
+    },
+
+    // Config for our build files
+    output: {
+        path: root('dist'),
+        filename: '[name].bundle.js',
+        sourceMapFilename: '[name].map',
+        chunkFilename: '[id].chunk.js'
+    },
 
     resolve: {
         // ensure loader extensions match
-        extensions: ['','.ts','.js','.json','.css','.html']
+        extensions: ['', '.ts', '.js', '.json', '.css', '.html']
     },
-  
-	module: {
-		loaders: [
-			{
-				test: /\.tsx?$/,
-				loader: 'ts'
-			}
-		]
-	},
-    
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new CopyWebpackPlugin([
-            { from: './index.html' },
-            { from: './favicon.ico' }
-        ]),
-        new BrowserSyncPlugin({
-            // browse to http://localhost:3000/ during development, 
-            // ./public directory is being served 
-            host: 'localhost',
-            port: 3000,
-            server: {
-                baseDir: ['build']
+
+    module: {
+        preLoaders: [{ test: /\.ts$/, loader: 'tslint-loader', exclude: [/node_modules/] }],
+        loaders: [
+            // Support for .ts files.
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                query: {
+                    'ignoreDiagnostics': [
+                        2403, // 2403 -> Subsequent variable declarations
+                        2300, // 2300 -> Duplicate identifier
+                        2374, // 2374 -> Duplicate number index signature
+                        2375  // 2375 -> Duplicate string index signature
+                    ]
+                },
+                exclude: [/\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/]
             },
-            browser: ['chrome']
+
+            // Support for *.json files.
+            { test: /\.json$/, loader: 'json-loader' },
+
+            // Support for CSS as raw text
+            { test: /\.css$/, loader: 'raw-loader' },
+
+            // support for .html as raw text
+            { test: /\.html$/, loader: 'raw-loader' }
+
+            // if you add a loader include the resolve file extension above
+        ]
+    },
+
+    plugins: [
+        new CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity }),
+        // static assets
+        new CopyWebpackPlugin([{ from: 'src/assets', to: 'assets' }]),
+        // generating html
+        new HtmlWebpackPlugin({ template: 'src/index.html', inject: false }),
+        // replace
+        new DefinePlugin({
+            'process.env': {
+                'ENV': JSON.stringify(metadata.ENV),
+                'NODE_ENV': JSON.stringify(metadata.ENV)
+            }
         })
-    ]
+    ],
+
+    // Other module loader config
+    tslint: {
+        emitErrors: false,
+        failOnHint: false
+    },
+    // our Webpack Development Server config
+    devServer: {
+        port: metadata.port,
+        host: metadata.host,
+        historyApiFallback: true,
+        watchOptions: { aggregateTimeout: 300, poll: 1000 }
+    },
+    // we need this due to problems with es6-shim
+    node: { global: 'window', progress: false, crypto: 'empty', module: false, clearImmediate: false, setImmediate: false }
+}
+
+function root(args) {
+    args = Array.prototype.slice.call(arguments, 0);
+    return path.join.apply(path, [__dirname].concat(args));
+}
+
+function rootNode(args) {
+    args = Array.prototype.slice.call(arguments, 0);
+    return root.apply(path, ['node_modules'].concat(args));
 }
